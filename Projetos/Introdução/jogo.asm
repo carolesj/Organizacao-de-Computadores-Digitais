7405556d5b64256d506efeb2f6584388a05925bc
Caroline Jesuíno <carolinejesuino.ns@gmail.com>
2017-10-03 22:36:04 -0300

    Criado o vetor de resposta e as telas de tabuleiro

---
+++ Projetos/Introdução/jogo.asm
@@ -0,0 +1,130 @@
+jmp main
+
+;define as posições da resposta na matriz da tela de output
+resposta_gde : var #58
+	static resposta_gde + #0, #208
+	static resposta_gde + #1, #209
+	static resposta_gde + #2, #245
+	static resposta_gde + #3, #246
+	static resposta_gde + #4, #247
+	static resposta_gde + #5, #248
+	static resposta_gde + #6, #251
+	static resposta_gde + #7, #285
+	static resposta_gde + #8, #290
+	static resposta_gde + #9, #327
+	static resposta_gde + #10, #328
+	static resposta_gde + #11, #330
+	static resposta_gde + #12, #332
+	static resposta_gde + #13, #333
+	static resposta_gde + #14, #346
+	static resposta_gde + #15, #347
+	static resposta_gde + #16, #348
+	static resposta_gde + #17, #349
+	static resposta_gde + #18, #350
+	static resposta_gde + #19, #351
+	static resposta_gde + #20, #352
+	static resposta_gde + #20, #353
+	static resposta_gde + #21, #354
+	static resposta_gde + #22, #386
+	static resposta_gde + #23, #387
+	static resposta_gde + #24, #388
+	static resposta_gde + #25, #389
+	static resposta_gde + #26, #390
+	static resposta_gde + #27, #391
+	static resposta_gde + #28, #392
+	static resposta_gde + #29, #394
+	static resposta_gde + #30, #426
+	static resposta_gde + #31, #427
+	static resposta_gde + #32, #428
+	static resposta_gde + #33, #429
+	static resposta_gde + #34, #430
+	static resposta_gde + #35, #431
+	static resposta_gde + #36, #432
+	static resposta_gde + #37, #433
+	static resposta_gde + #38, #434
+	static resposta_gde + #39, #466
+	static resposta_gde + #40, #467
+	static resposta_gde + #41, #468
+	static resposta_gde + #42, #469
+	static resposta_gde + #43, #470
+	static resposta_gde + #44, #471
+	static resposta_gde + #45, #473
+	static resposta_gde + #46, #474
+	static resposta_gde + #47, #507
+	static resposta_gde + #48, #508
+	static resposta_gde + #49, #509
+	static resposta_gde + #50, #510
+	static resposta_gde + #51, #512
+	static resposta_gde + #52, #513
+	static resposta_gde + #53, #548
+	static resposta_gde + #54, #549
+	static resposta_gde + #55, #550
+	static resposta_gde + #56, #551
+	static resposta_gde + #57, #552
+		
+
+		
+	
+	
+	
+tela1Linha00 : string "           1                            "
+tela1Linha01 : string "                                        "
+tela1Linha02 : string "      1221 442                          "
+tela1Linha03 : string "                                        "
+tela1Linha04 : string "     2467681234                        "
+tela1Linha05 : string "    2                                   "
+tela1Linha06 : string "  4 1                                   "
+tela1Linha07 : string "  1 1                                   "
+tela1Linha08 : string "2 1 2                                   "
+tela1Linha09 : string "    9                                   "
+tela1Linha10 : string "  7 1                                   "
+tela1Linha11 : string "    9                                   "
+tela1Linha12 : string "  6 2                                   "
+tela1Linha13 : string "  4 2                                   "
+tela1Linha14 : string "    5                                   "
+tela1Linha15 : string "                                        "
+tela1Linha16 : string "                                        "
+tela1Linha17 : string "                                        "
+tela1Linha18 : string "                                        "
+tela1Linha19 : string "                                        "
+tela1Linha20 : string "                                        "
+tela1Linha21 : string "                                        "
+tela1Linha22 : string "                                        "
+tela1Linha23 : string "                                        "
+tela1Linha24 : string "                                        "
+tela1Linha25 : string "                                        "
+tela1Linha26 : string "                                        "
+tela1Linha27 : string "                                        "
+tela1Linha28 : string "                                        "
+tela1Linha29 : string "                                        "
+
+tela1Linha00 : string "                                        "
+tela1Linha01 : string "                                        "
+tela1Linha02 : string "                                        "
+tela1Linha03 : string "                                        "
+tela1Linha04 : string "                                        "
+tela1Linha05 : string "     ##########                         "
+tela1Linha06 : string "     ##########                         "
+tela1Linha07 : string "     ##########                         "
+tela1Linha08 : string "     ##########                         "
+tela1Linha09 : string "     ##########                         "
+tela1Linha10 : string "     ##########                         "
+tela1Linha11 : string "     ##########                         "
+tela1Linha12 : string "     ##########                         "
+tela1Linha13 : string "     ##########                         "
+tela1Linha14 : string "     ##########                         "
+tela1Linha15 : string "                                        "
+tela1Linha16 : string "                                        "
+tela1Linha17 : string "                                        "
+tela1Linha18 : string "                                        "
+tela1Linha19 : string "                                        "
+tela1Linha20 : string "                                        "
+tela1Linha21 : string "                                        "
+tela1Linha22 : string "                                        "
+tela1Linha23 : string "                                        "
+tela1Linha24 : string "                                        "
+tela1Linha25 : string "                                        "
+tela1Linha26 : string "                                        "
+tela1Linha27 : string "                                        "
+tela1Linha28 : string "                                        "
+tela1Linha29 : string "                                        "
