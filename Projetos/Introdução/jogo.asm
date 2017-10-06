ec73947c51523ff1b294f2f369bfc10e055da825
carolesj <caroline.jesuino.silva@usp.br>
2017-10-06 19:57:33 -0300

    I have no idea what I am doing

--- Projetos/Introdução/jogo.asm
+++ Projetos/Introdução/jogo.asm
@@ -98,33 +98,91 @@ tela1Linha27 : string "                                        "
 tela1Linha28 : string "                                        "
 tela1Linha29 : string "                                        "
 
-tela1Linha00 : string "                                        "
-tela1Linha01 : string "                                        "
-tela1Linha02 : string "                                        "
-tela1Linha03 : string "                                        "
-tela1Linha04 : string "                                        "
-tela1Linha05 : string "     ##########                         "
-tela1Linha06 : string "     ##########                         "
-tela1Linha07 : string "     ##########                         "
-tela1Linha08 : string "     ##########                         "
-tela1Linha09 : string "     ##########                         "
-tela1Linha10 : string "     ##########                         "
-tela1Linha11 : string "     ##########                         "
-tela1Linha12 : string "     ##########                         "
-tela1Linha13 : string "     ##########                         "
-tela1Linha14 : string "     ##########                         "
-tela1Linha15 : string "                                        "
-tela1Linha16 : string "                                        "
-tela1Linha17 : string "                                        "
-tela1Linha18 : string "                                        "
-tela1Linha19 : string "                                        "
-tela1Linha20 : string "                                        "
-tela1Linha21 : string "                                        "
-tela1Linha22 : string "                                        "
-tela1Linha23 : string "                                        "
-tela1Linha24 : string "                                        "
-tela1Linha25 : string "                                        "
-tela1Linha26 : string "                                        "
-tela1Linha27 : string "                                        "
-tela1Linha28 : string "                                        "
-tela1Linha29 : string "                                        "
+tela2Linha00 : string "                                        "
+tela2Linha01 : string "                                        "
+tela2Linha02 : string "                                        "
+tela2Linha03 : string "                                        "
+tela2Linha04 : string "                                        "
+tela2Linha05 : string "     ##########                         "
+tela2Linha06 : string "     ##########                         "
+tela2Linha07 : string "     ##########                         "
+tela2Linha08 : string "     ##########                         "
+tela2Linha09 : string "     ##########                         "
+tela2Linha10 : string "     ##########                         "
+tela2Linha11 : string "     ##########                         "
+tela2Linha12 : string "     ##########                         "
+tela2Linha13 : string "     ##########                         "
+tela2Linha14 : string "     ##########                         "
+tela2Linha15 : string "                                        "
+tela2Linha16 : string "                                        "
+tela2Linha17 : string "                                        "
+tela2Linha18 : string "                                        "
+tela2Linha19 : string "                                        "
+tela2Linha20 : string "                                        "
+tela2Linha21 : string "                                        "
+tela2Linha22 : string "                                        "
+tela2Linha23 : string "                                        "
+tela2Linha24 : string "                                        "
+tela2Linha25 : string "                                        "
+tela2Linha26 : string "                                        "
+tela2Linha27 : string "                                        "
+tela2Linha28 : string "                                        "
+tela2Linha29 : string "                                        "
+
+main: 
+	loadn R0, #0  ; posicao inicial tem que ser o comeco da tela!
+	loadn R1, tela1Linha0
+	loadn R2, #0  ; cor branca!
+	Loop:
+		call ImprimeStr
+		;incrementa posicao para a proxima linha
+		add r0, r0, 40  
+		;incrementa o ponteiro para o comeco da proxima linha na memoria (40 + 1 por causa do /0 !!)
+		add r1, r1, 41  
+		cmp r0, 1200
+		jne Loop
+	rts
+	loadn R0, #0
+	loadn R1, tela2Linha0
+	;branco
+	loadn R2, #0  
+	call Loop
+	
+		
+	ImprimeStr2:	;  Rotina de Impresao de Mensagens:    r0 = Posicao da tela que o primeiro caractere da mensagem sera' impresso;  r1 = endereco onde comeca a mensagem; r2 = cor da mensagem.   Obs: a mensagem sera' impressa ate' encontrar "/0"
+		push r0	; protege o r0 na pilha para preservar seu valor
+		push r1	; protege o r1 na pilha para preservar seu valor
+		push r2	; protege o r1 na pilha para preservar seu valor
+		push r3	; protege o r3 na pilha para ser usado na subrotina
+		push r4	; protege o r4 na pilha para ser usado na subrotina
+		push r5	; protege o r5 na pilha para ser usado na subrotina
+		push r6	; protege o r6 na pilha para ser usado na subrotina
+		
+		
+		loadn r3, #'\0'	; Criterio de parada
+		loadn r5, #' '	; Espaco em Branco
+
+	ImprimeStr2_Loop:	
+		loadi r4, r1
+		cmp r4, r3		; If (Char == \0)  vai Embora
+		jeq ImprimeStr2_Sai
+		cmp r4, r5		; If (Char == ' ')  vai Pula outchar do espaco para na apagar outros caracteres
+		jeq ImprimeStr2_Skip
+		add r4, r2, r4	; Soma a Cor
+		outchar r4, r0	; Imprime o caractere na tela
+		storei r6, r4
+	ImprimeStr2_Skip:
+		inc r0			; Incrementa a posicao na tela
+		inc r1			; Incrementa o ponteiro da String
+		inc r6			; Incrementa o ponteiro da String da Tela 0
+		jmp ImprimeStr2_Loop
+		
+	ImprimeStr2_Sai:	
+		pop r6	; Resgata os valores dos registradores utilizados na Subrotina da Pilha
+		pop r5
+		pop r4
+		pop r3
+		pop r2
+		pop r1
+		pop r0
+		rts
